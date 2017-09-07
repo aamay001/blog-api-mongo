bb51e402872de851ffc59a73120f9910deea4aec
Andy Amaya <aamay001@gmail.com>
2017-09-07 10:53:23 -0700

    server test for init

---
+++ test/test-server.js
@@ -0,0 +1,26 @@
+// Simple test to make sure the server starts and stops
+
+const chai = require('chai');
+const should = chai.should();
+
+const { runServer, closeServer } = require('../server');
+
+describe( 'Start Server', function(){
+  it('should start the server', function(){
+    return runServer()
+      .then(function(res){
+        res.status.should.be.a('String');
+        res.status.should.deep.equal('OK');
+      });
+  });
+});
+
+describe('Close Server', function(){
+  it('should close the server', function() {
+    return closeServer()
+      .then(function(res){
+        res.status.should.be.a('String');
+        res.status.should.deep.equal('OK');
+      });
+  });
+});
